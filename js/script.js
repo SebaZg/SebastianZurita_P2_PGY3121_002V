a70d1db8a944ac4840bfa3720faaaeaac9827bc6
SebaZg <seba.zurita@duocuc.cl>
2022-06-16 13:09:15 -0400

    COMMIT3

--- js/script.js
+++ js/script.js
@@ -2,16 +2,10 @@ function getBasePath(service) {
     return "https://chilealerta.com/api/query/?user=demo&select=" + service;
 }
 
-function getBasePath(service) {
-    return "https://chilealerta.com/api/query/?user=demo&select=onemi=" + service;
+function createRowDisaster(dato1, dato2, dato3, dato4, dato5) {
+    return '<tr><td>' + dato1 + '</td><td>' + dato2 + '</td><td>' + dato3 + '</td><td>' + dato4 + '</td><td>' + dato5 + '</td></tr>';
 }
 
-function getBasePath(service) {
-    return "https://chilealerta.com/api/query/?user=demo&select=ultimos_sismos=" + service;
+function createRowDisasterUrl(dato1, dato2, dato3, dato4, dato5) {
+    return '<tr><td>' + dato1 + '</td><td>' + dato2 + '</td><td>' + dato3 + '</td><td>' + dato4 + '</td><td><a href=' + dato5 + '>Mas Info</a></td></tr>';
 }
-
-function createRowDisaster(reference, magnitude, latitude, longitude, local_time) {
-    return '<tr><td>' + reference + '</td><td>' + magnitude + '</td><td>' + latitude + '</td><td>' + longitude + '</td><td>' + local_time + '</td></tr>';
-}
-
-
